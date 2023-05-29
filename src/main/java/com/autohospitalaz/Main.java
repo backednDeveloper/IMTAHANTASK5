b0dc056cc09918033c3e120f2f82ce15bf12cbde
Bank System <emilaze77@gmail.com>
2023-05-29 21:28:42 +0500

    Imtahantask

--- src/main/java/com/autohospitalaz/Main.java
+++ src/main/java/com/autohospitalaz/Main.java
@@ -1,7 +1,45 @@
 package com.autohospitalaz;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-       
-        }
+    public static void main(String[] args) throws IOException {
+        List<Integer> list1 = Arrays.asList(26,56,21,33,99,45,9,18,122);
+        System.out.println(Methods.bolunenMethodu(list1));
+        System.out.println("----------------------");
+        System.out.println(Methods.findAvrage(list1));
+        System.out.println("----------------------");
+        String str = "Div-Academy-2023-Java";
+        System.out.println(Methods.splitMethod(str));
+        System.out.println("----------------------");
+        int average = Methods.findMaxInt(list1) + Methods.findMinInt(list1) / 2;
+        System.out.println(average);
+        System.out.println("----------------------");
+        String  string  = "    Hello World   ";
+        System.out.println(Methods.deleteAbsent(string));
+        System.out.println("----------------------");
+        File file = new File("File");
+        try{
+        Methods.fileMethod(file);
+        }catch (FileNotFoundException e){
+            e.getMessage();
+            }
+        String java = "Java is a programing language";
+        System.out.println(Methods.arrayDoldurmag(java));
+        System.out.println("----------------------");
+        List<String> array = new ArrayList<>();
+        array.add(new String("Java"));
+        array.add(new String("Astonmartin"));
+        array.add(new String("Jeva"));
+        System.out.println(Methods.stringarrayDetected(array));
+        System.out.println("----------------------");
+        System.out.println(Methods.stringLenght(array));
+        System.out.println("----------------------");
+    }
+
     }
---
+++ src/main/java/com/autohospitalaz/Methods.java
@@ -0,0 +1,69 @@
+package com.autohospitalaz;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Methods {
+    public static Integer findMinInt(List<Integer> list) {
+        Optional<Integer> min = list.stream()
+                .min(Integer::compareTo);
+        return min.orElse(null);
+    }
+    public static Integer findMaxInt(List<Integer> list) {
+        Optional<Integer> max = list.stream()
+                .max(Integer::compareTo);
+        return max.orElse(null);
+    }
+    public static Double findAvrage(List<Integer> list) {
+        return list.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0.0);
+    }
+    public static boolean fileMethod(File file) throws IOException {
+        boolean created = false;
+        if (!file.exists()) {
+            created = file.createNewFile();
+        }
+        return created;
+    }
+    public static List<Integer> bolunenMethodu(List<Integer>list){
+        return list.stream()
+                .filter(s-> s%3==0)
+                .collect(Collectors.toList());
+    }
+    public static List<String> splitMethod (String title){
+        return Arrays.stream(title.split("-")).map(String::toString).collect(Collectors.toList());
+    }
+    public static String deleteAbsent(String title){
+        return Arrays.stream(new String[]{title.trim().replace(" ", " ")}).map(String::toString).collect(Collectors.toList()).toString();
+    }
+    public static String stringUpdate(List<String> list){
+        return stringUpdate(list);
+    }
+    public static Double findAverage1(List<Integer>list){
+        return list.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0.0);
+    }
+    public static String arrayDoldurmag(String title){
+        return Arrays.stream(title.split(" ")).map(String::toString).collect(Collectors.toList()).toString();
+    }
+    public static String stringarrayDetected(List<String>list){
+         return list.stream()
+                 .filter(s-> s.toUpperCase().startsWith("J") && s.toUpperCase().endsWith("A")).collect(Collectors.toList()).toString();
+    }
+    public static Integer stringLenght(List<String>list){
+        return list.stream()
+                .map(s-> s.length())
+                .reduce((integer, integer2) -> integer + integer2).orElse(0);
+    }
+
+
+
+}
