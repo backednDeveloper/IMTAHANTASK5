a1c92c5a6f5241b240cc93444f7c584b45215e4e
Bank System <emilaze77@gmail.com>
2023-05-30 16:05:15 +0500

    Imtahantask

--- src/main/java/com/autohospitalaz/Methods.java
+++ src/main/java/com/autohospitalaz/Methods.java
@@ -13,17 +13,20 @@ public class Methods {
                 .min(Integer::compareTo);
         return min.orElse(null);
     }
+
     public static Integer findMaxInt(List<Integer> list) {
         Optional<Integer> max = list.stream()
                 .max(Integer::compareTo);
         return max.orElse(null);
     }
+
     public static Double findAvrage(List<Integer> list) {
         return list.stream()
                 .mapToInt(Integer::intValue)
                 .average()
                 .orElse(0.0);
     }
+
     public static boolean fileMethod(File file) throws IOException {
         boolean created = false;
         if (!file.exists()) {
@@ -31,36 +34,40 @@ public class Methods {
         }
         return created;
     }
-    public static List<Integer> bolunenMethodu(List<Integer>list){
+
+    public static List<Integer> bolunenMethodu(List<Integer> list) {
         return list.stream()
-                .filter(s-> s%3==0)
+                .filter(s -> s % 3 == 0)
                 .collect(Collectors.toList());
     }
-    public static List<String> splitMethod (String title){
+
+    public static List<String> splitMethod(String title) {
         return Arrays.stream(title.split("-")).map(String::toString).collect(Collectors.toList());
     }
-    public static String deleteAbsent(String title){
-        return Arrays.stream(new String[]{title.trim().replace(" ", " ")}).map(String::toString).collect(Collectors.toList()).toString();
+
+    public static List<String> deleteAbsent(String title) {
+        return Arrays.stream(new String[]{title.trim().replace(" ", " ")}).map(String::toString).collect(Collectors.toList());
     }
-    public static String stringUpdate(List<String> list){
-        return stringUpdate(list);
-    }
-    public static Double findAverage1(List<Integer>list){
+
+    public static Double findAverage1(List<Integer> list) {
         return list.stream()
                 .mapToInt(Integer::intValue)
                 .average()
                 .orElse(0.0);
     }
-    public static String arrayDoldurmag(String title){
-        return Arrays.stream(title.split(" ")).map(String::toString).collect(Collectors.toList()).toString();
+
+    public static List<String> arrayDoldurmag(String title) {
+        return Arrays.stream(title.split(" ")).map(String::toString).collect(Collectors.toList());
     }
-    public static String stringarrayDetected(List<String>list){
-         return list.stream()
-                 .filter(s-> s.toUpperCase().startsWith("J") && s.toUpperCase().endsWith("A")).collect(Collectors.toList()).toString();
-    }
-    public static Integer stringLenght(List<String>list){
+
+    public static List<String> stringarrayDetected(List<String> list) {
         return list.stream()
-                .map(s-> s.length())
+                .filter(s -> s.toUpperCase().startsWith("J") && s.toUpperCase().endsWith("A")).collect(Collectors.toList());
+    }
+
+    public static Integer stringLenght(List<String> list) {
+        return list.stream()
+                .map(s -> s.length())
                 .reduce((integer, integer2) -> integer + integer2).orElse(0);
     }
 
